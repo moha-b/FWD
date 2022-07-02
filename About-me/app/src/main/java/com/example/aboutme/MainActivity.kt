7cfca9ee443057c1cfe78a28c78fc5086961fe86
hab <73842931+moha-b@users.noreply.github.com>
2022-07-02 14:19:53 +0200

    data Binding

--- About-me/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ About-me/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //lateinit var done:Button
+    //lateinit var typeName:EditText
+    //lateinit var showName:TextView
+    private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+//        done = findViewById(R.id.done)
+//        done.setOnClickListener {
+//            addNicName(it)
+//        }
+        bind.done.setOnClickListener {
+            addNicName(it)
+        }
+    }
+    private fun addNicName(view:View){
+
+        bind.apply {
+            bind.showName.text = bind.nickName.text
+            invalidateAll()
+            bind.nickName.visibility = View.GONE
+            bind.done.visibility = View.GONE
+            bind.showName.visibility = View.VISIBLE
+        }
+//        typeName = findViewById(R.id.nickName)
+//        showName = findViewById(R.id.showName)
+//
+//        showName.text = typeName.text
+//
+//        typeName.visibility = View.GONE
+//        view.visibility = View.GONE
+//        showName.visibility = View.VISIBLE
     }
 }
